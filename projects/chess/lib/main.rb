77d3ef82acebe60054374c3572a5465d67a1d52b
Daniel <d-abraham@live.co.uk>
2024-01-29 15:03:05 +0000

    Fixed bug in chess code which caused a promoted pawn to checkmate the enemy king when adjacent. I had a break in the code if it saw a piece that wasn't friendly, causing the code to only check legal moves for a few pieces instead of all of them, specifically the king didn't get looked at while having the legal to move to simply take the promoted queen next t it.

--- projects/chess/lib/main.rb
+++ projects/chess/lib/main.rb
@@ -7,7 +7,7 @@ include Coordinates
 
 class Chess
   attr_accessor :board, :valid_squares, :position, :position_index, :white_pieces, :black_pieces, :valid_moves, :letters_to_coords, :selected_piece, :white_king_position, :black_king_position, :turn, :to_play, :piece_positions, :last_piece_moved, :has_moved
-  def initialize(board = Array.new(4) {Array.new(8, "_")}, turn = 1, to_play = "white", last_piece_moved = nil, has_moved = {black_a_rook: "no", black_h_rook: "no", black_king: "no", white_a_rook: "no", white_h_rook: "no", white_king: "no"})
+  def initialize(board = Array.new(4) {Array.new(8, "_")}, turn = 1, to_play = "white", last_piece_moved = nil, has_moved = {black_a_rook: "no", black_h_rook: "no", black_king: "no", white_a_rook: "no", white_h_rook: "no", white_king: "no"}, white_king_position = [7, 0], black_king_position = [0, 4])
     @board = board
     @turn = turn
     @to_play = to_play
@@ -21,8 +21,8 @@ class Chess
     @valid_moves = []
     @letters_to_coords = %w[a b c d e f g h]
     @selected_piece = nil
-    @white_king_position = [7, 4]
-    @black_king_position = [0, 4]
+    @white_king_position = white_king_position
+    @black_king_position = black_king_position
     @piece_positions = {}
     if @board.size == 4
       @board << %w[♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙]
@@ -40,7 +40,7 @@ class Chess
   end
 
   def choose_square
-    puts "Please choose a piece to move (a1 - h8) or type 'save"
+    puts "Please choose a piece to move (a1 - h8) or type 'save'"
     answer = gets.chomp
 
     unless @valid_squares.include?(answer) || answer.downcase == 'save'
@@ -113,6 +113,23 @@ class Chess
   def pawn_options(movement_coords, attack_coords)
     self.valid_moves = []
 
+    attack_coords.each do |a_coords|
+      if self.valid_squares.include?(coords_to_square([position_index[1] + a_coords[1], position_index[0] + a_coords[0]]))
+        enemies = self.white_pieces.include?(self.selected_piece) ? self.black_pieces : self.white_pieces
+
+        square_being_looked_at = self.board[position_index[1] + a_coords[1]][position_index[0] + a_coords[0]]
+
+        unless position_index[1] + a_coords[1] < 0 || position_index[0] + a_coords[0] < 0
+
+          if enemies.include?(square_being_looked_at)
+
+            self.valid_moves << coords_to_square([position_index[1] + a_coords[1], position_index[0] + a_coords[0]])
+
+          end
+        end
+      end
+    end
+
     movement_coords.each do |coords|
 
       if self.board[position_index[1] + coords[1]][position_index[0] + coords[0]] == "_" # Adding the relative valid attack square index via 'coords'
@@ -120,7 +137,7 @@ class Chess
         self.valid_moves << coords_to_square([position_index[1] + coords[1], position_index[0] + coords[0]])
 
       else
-        break
+
       end
 
       if @selected_piece == "♙" && position_index[1] != 6
@@ -129,18 +146,7 @@ class Chess
 
       if @selected_piece == "♟︎" && position_index[1] != 1
         break
-      end
-    end
-
-    attack_coords.each do |coords|
-      enemies = self.white_pieces.include?(self.selected_piece) ? self.black_pieces : self.white_pieces
-
-      square_being_looked_at = self.board[position_index[1] + coords[1]][position_index[0] + coords[0]]
-      if enemies.include?(square_being_looked_at)
-
-        self.valid_moves << coords_to_square([position_index[1] + coords[1], position_index[0] + coords[0]])
-
-      end
+      end 
     end
   end
 
@@ -255,9 +261,10 @@ class Chess
 
       if self.valid_squares.include?(coords_to_square([white_king_position[0] + coords[1], white_king_position[0] + coords[1]]))
 
-        square_being_looked_at = board[white_king_position[0] + coords[1]][white_king_position[1] + coords[0]]
+        square_being_looked_at = board[white_king_position[1] + coords[1]][white_king_position[0] + coords[0]]
 
         if %w[♟︎].include?(square_being_looked_at)
+          # pp "found pawn check"
           in_check = true
           break
         end
@@ -272,6 +279,7 @@ class Chess
         square_being_looked_at = board[white_king_position[0] + coords[1]][white_king_position[1] + coords[0]]
 
         if %w[♞].include?(square_being_looked_at)
+          # pp "found knight check"
           in_check = true
           break
         end
@@ -286,6 +294,7 @@ class Chess
         square_being_looked_at = board[white_king_position[0] + coords[1]][black_king_position[1] + coords[0]]
 
         if %w[♚].include?(square_being_looked_at)
+          # pp "found king check"
           in_check = true
           break
         end
@@ -299,11 +308,15 @@ class Chess
         if self.valid_squares.include?(coords_to_square([white_king_position[0] + coords[1], white_king_position[1] + coords[0]]))
           square_being_looked_at = board[white_king_position[0] + coords[1]][white_king_position[1] + coords[0]]
 
-          if %w[♜ ♛].include?(square_being_looked_at)
-            in_check = true
-            break
-          elsif friends.include?(square_being_looked_at)
-            break
+          unless white_king_position[0] + coords[1] < 0 || white_king_position[1] + coords[0] < 0
+       
+            if %w[♜ ♛].include?(square_being_looked_at)
+              # pp "found rook/queen check"
+              in_check = true
+              break
+            elsif friends.include?(square_being_looked_at) || %w[♟︎ ♞ ♝ ♚].include?(square_being_looked_at)
+              break
+            end
           end
         end
       end
@@ -321,9 +334,10 @@ class Chess
           end
 
           if %w[♝ ♛].include?(square_being_looked_at)
+            # pp "found bishop/queen check"
             in_check = true
             break
-          elsif friends.include?(square_being_looked_at)
+          elsif friends.include?(square_being_looked_at) || %w[♟︎ ♞ ♜ ♚].include?(square_being_looked_at)
             break
           end
         end
@@ -345,6 +359,7 @@ class Chess
         square_being_looked_at = board[black_king_position[0] + coords[1]][black_king_position[1] + coords[0]]
 
         if %w[♙].include?(square_being_looked_at)
+          # pp "found pawn check"
           in_check = true
           break
         end
@@ -359,6 +374,7 @@ class Chess
         square_being_looked_at = board[black_king_position[0] + coords[1]][black_king_position[1] + coords[0]]
 
         if %w[♘].include?(square_being_looked_at)
+          # pp "found knight check"
           in_check = true
           break
         end
@@ -373,6 +389,7 @@ class Chess
         square_being_looked_at = board[black_king_position[0] + coords[1]][black_king_position[1] + coords[0]]
 
         if %w[♔].include?(square_being_looked_at)
+          # pp "found king check"
           in_check = true
           break
         end
@@ -386,11 +403,14 @@ class Chess
         if self.valid_squares.include?(coords_to_square([black_king_position[0] + coords[1], black_king_position[1] + coords[0]]))
           square_being_looked_at = board[black_king_position[0] + coords[1]][black_king_position[1] + coords[0]]
 
-          if %w[♖ ♕].include?(square_being_looked_at)
-            in_check = true
-            break
-          elsif friends.include?(square_being_looked_at)
-            break
+          unless black_king_position[0] + coords[1] < 0 || black_king_position[1] + coords[0] < 0
+            if %w[♖ ♕].include?(square_being_looked_at)
+              # pp "found rook/queen check"
+              in_check = true
+              break
+            elsif friends.include?(square_being_looked_at) || %w[♙ ♘ ♗ ♔].include?(square_being_looked_at)
+              break
+            end
           end
         end
       end
@@ -408,9 +428,10 @@ class Chess
           end
 
           if %w[♗ ♕].include?(square_being_looked_at)
+            # pp "found bishop/queen check"
             in_check = true
             break
-          elsif friends.include?(square_being_looked_at)
+          elsif friends.include?(square_being_looked_at) || %w[♙ ♘ ♖ ♔].include?(square_being_looked_at)
             break
           end
         end
@@ -488,11 +509,70 @@ class Chess
       @black_king_position[1] = answer_index[0]
     end
 
-    self.board[answer_index[1]][answer_index[0]] = @selected_piece
+    if @selected_piece == "♙"
 
-    self.board[position_index[1]][position_index[0]] = "_"
+      if answer_index[1] == 0
 
+        self.promotion("white", answer_index[0])
+
+      else
+        self.board[answer_index[1]][answer_index[0]] = @selected_piece
+
+        self.board[position_index[1]][position_index[0]] = "_"
+      end
+
+    elsif @selected_piece == "♟︎"
+
+      if answer_index[1] == 7
+
+        self.promotion("black", answer_index[0])
+
+      else
+        self.board[answer_index[1]][answer_index[0]] = @selected_piece
+
+        self.board[position_index[1]][position_index[0]] = "_"
+      
+      end
+
+    else
+      self.board[answer_index[1]][answer_index[0]] = @selected_piece
+
+      self.board[position_index[1]][position_index[0]] = "_"
+    
+    end
   end
+
+  def promotion(colour, file)
+
+    puts "Choose which piece to promote your pawn to [Queen / Rook / Knight / Bishop]"
+
+    answer = gets.chomp
+
+    promotion_pieces = %w[queen rook knight bishop]
+
+    white_promotions = {"queen" => "♕", "rook" => "♖", "knight" => "♘", "bishop" => "♗"}
+
+    black_promotions = {"queen" => "♛", "rook" => "♜", "knight" => "♞", "bishop" => "♝"}
+
+
+    unless promotion_pieces.include?(answer.downcase)
+      puts "Must pick a valid piece"
+      return self.promotion
+    end
+
+    case @to_play
+    when "white"
+
+      @board[0][file] = white_promotions[answer.to_s]
+      @board[position_index[1]][position_index[0]] = "_"
+    when "black"
+      @board[7][file] = black_promotions[answer.to_s]
+      @board[position_index[1]][position_index[0]] = "_"
+    end
+  end
+
+
+
 
   def fetch_piece_positions
     @board.each_with_index do |row, row_index|
@@ -500,8 +580,6 @@ class Chess
         friends = @to_play == "white" ? @white_pieces : @black_pieces
         if friends.include?(column)
           @piece_positions[self.coords_to_square([row_index, column_index])] = column
-        else
-          break
         end
       end
     end
@@ -550,7 +628,9 @@ class Chess
     end
 
     self.choose_square
+    pp @valid_moves
     self.check_valid_moves
+    pp @valid_moves
     if @valid_moves.empty?
       puts "No legal moves for this piece"
       return self.play_game
@@ -578,43 +658,44 @@ class Chess
      turn: @turn, 
      to_play: @to_play,
      last_piece_moved: @last_piece_moved,
-     has_moved: @has_moved }
+     has_moved: @has_moved,
+     white_king_position: @white_king_position,
+     black_king_position: @black_king_position }
     serialized_game = YAML::dump(game_hash)
     File.write("saves/#{game_name}.yaml", serialized_game)
     puts "Game saved successfully as #{game_name}.yaml"
     exit
   end
-
-  def new_game_or_load
-    puts "Would you like to start a new game, or load one? [new / load]"
-    answer = gets.chomp
-    unless answer.downcase == 'new' || answer.downcase == 'load'
-      return self.new_game_or_load
-    end
-    case answer.downcase
-    when 'new'
-      self.play_game
-    when 'load'
-      self.load_game
-    end
-  end
-
-  def load_game
-    puts Dir.entries("saves")
-    save_files =[]
-    save_files << Dir.entries("saves")
-    pp save_files
-    puts "Please pick a game to load"
-    response = gets.chomp
-    serialized_game = File.open("saves/#{response}", "r")
-    unserialized_game = YAML::load_file(serialized_game)
-    loaded_game = Chess.new(unserialized_game[:board], unserialized_game[:turn], unserialized_game[:to_play], unserialized_game[:last_piece_moved], unserialized_game[:has_moved])
-    loaded_game.play_game
-  rescue Errno::ENOENT
-    self.load_game
-  end
-
 end
 
-game = Chess.new
-game.new_game_or_load
+def new_game_or_load
+  puts "Would you like to start a new game, or load one? [new / load]"
+  answer = gets.chomp
+  unless answer.downcase == 'new' || answer.downcase == 'load'
+    return self.new_game_or_load
+  end
+  case answer.downcase
+  when 'new'
+    game = Chess.new
+    game.play_game
+  when 'load'
+    self.load_game
+  end
+end
+
+def load_game
+  puts Dir.entries("saves")
+  save_files =[]
+  save_files << Dir.entries("saves")
+  pp save_files
+  puts "Please pick a game to load"
+  response = gets.chomp
+  serialized_game = File.open("saves/#{response}", "r")
+  unserialized_game = YAML::load_file(serialized_game)
+  loaded_game = Chess.new(unserialized_game[:board], unserialized_game[:turn], unserialized_game[:to_play], unserialized_game[:last_piece_moved], unserialized_game[:has_moved], unserialized_game[:white_king_position], unserialized_game[:black_king_position])
+  loaded_game.play_game
+rescue Errno::ENOENT
+  self.load_game
+end
+
+new_game_or_load()
