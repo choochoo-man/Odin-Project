cb2b4a460baf954f0c9301f91e273cefd32d21d5
Daniel <d-abraham@live.co.uk>
2024-01-29 18:16:23 +0000

    begun splitting out methods in chess so that I can store them in different classes within different files.

--- projects/chess/lib/main.rb
+++ projects/chess/lib/main.rb
@@ -167,6 +167,24 @@ class Chess
     end
   end
 
+  def king_options(relative_coords)
+    self.valid_moves = []
+    relative_coords.each do |coords|
+      enemies = self.white_pieces.include?(self.selected_piece) ? self.black_pieces : self.white_pieces
+      if self.valid_squares.include?(coords_to_square([position_index[1] + coords[1], position_index[0] + coords[0]]))
+
+        square_being_looked_at = self.board[position_index[1] + coords[1]][position_index[0] + coords[0]]
+
+        if enemies.include?(square_being_looked_at) || square_being_looked_at == "_"
+
+          self.valid_moves << coords_to_square([position_index[1] + coords[1], position_index[0] + coords[0]])
+
+        end
+      end
+    end
+    
+  end
+
   def iterative_options(relative_coords)
     self.valid_moves = []
     relative_coords.each do |directions|
@@ -235,7 +253,7 @@ class Chess
 
   def white_king
     relative_coords = Coordinates.king_moves
-      knight_options(relative_coords)
+      king_options(relative_coords)
   end
 
   def black_pawn
@@ -248,7 +266,7 @@ class Chess
 
   def black_king
      relative_coords = Coordinates.king_moves
-     knight_options(relative_coords)
+     king_options(relative_coords)
   end
 
   def white_king_check?(board = @board, white_king_position = @white_king_position)
